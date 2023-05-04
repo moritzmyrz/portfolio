831b7b04a21e03676c71077a96e0dbe7f7cc943d
Moritz André Myrseth <moritzmyrz@gmail.com>
2023-05-04 15:50:11 +0200

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	import Navbar from '../components/Navbar.svelte';
 	import '../global.css';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({mode: dev ? 'development' : 'production'})
 </script>
 
 <Navbar />

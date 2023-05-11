b3c03d41e318e92f37511dab332e03725da85cee
Moritz André Myrseth <moritzmyrz@gmail.com>
2023-05-11 23:22:02 +0200

    sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,16 +1,46 @@
+import { getProjectSlugs } from '$lib/utils/sanity';
+
+function formatDate(date: Date) {
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+
+	return `${year}-${month}-${day}`;
+}
+
 export async function GET() {
+	const lastmod = formatDate(new Date());
+
+	const slugs: string[] = await getProjectSlugs();
 	return new Response(
 		`
         <?xml version="1.0" encoding="UTF-8" ?>
         <urlset
             xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:xhtml="https://www.w3.org/1999/xhtml"
-            xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-            xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-            xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-            xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
         >
-            <!-- <url> elements go here -->
+            <url>
+                <loc>https://www.myrseth.dev/</loc>
+                <lastmod>${lastmod}</lastmod>
+                <changefreq>always</changefreq>
+                <priority>1.0</priority>
+            </url>
+            <url>
+                <loc>https://www.myrseth.dev/prosjekter/</loc>
+                <lastmod>${lastmod}</lastmod>
+                <changefreq>always</changefreq>
+                <priority>0.8</priority>
+            </url>
+            ${slugs.map(
+							(slug) =>
+								`
+                    <url>
+                        <loc>https://www.myrseth.dev/prosjekter/${slug}</loc>
+                        <lastmod>${lastmod}</lastmod>
+                        <changefreq>always</changefreq>
+                        <priority>0.8</priority>
+                    </url>
+                `
+						)}
         </urlset>`.trim(),
 		{
 			headers: {
